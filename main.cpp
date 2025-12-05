2584167d0836747d1cda88e724c0cb64ea9cf464
Giulio <giuliopecorini@outlook.it>
2025-12-05 16:38:32 +0100

    riassuntivo gestione di a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 // Programa d'informatica
 // Attenzione questo file è a solo scopo istituzionale
+
+
 int main(){
+	int a = 0;
+	cin >> a;
+	if (a<10){
+		return a;
+	}
 	cout << "hello world""<< endl;
 }
 
